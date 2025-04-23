0c43afa9d80b5441c5a21bd3da1876ae1cc61840
Xiaohui-Hu <hxhpx0208@163.com>
2025-04-23 13:56:27 +0800

    change font

--- sync-wiki.sh
+++ sync-wiki.sh
@@ -3,10 +3,12 @@
 # This script can be used to sync the wiki submodule
 # in case you're using github internal wiki option
 #
+# git pull origin master
+# cd wiki
+# git pull origin master
 
-git pull origin master
-cd wiki
-git pull origin master
-cd ..
-git commit -a -m "updated wiki"
+#!/bin/bash
+
+git add .
+git commit -m "update"
 git push origin master
